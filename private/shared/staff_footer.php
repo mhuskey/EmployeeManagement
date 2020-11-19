2fbac9506dafeee91b8f717375da4a03ab2fc4be
Matthew Huskey <30731520+mhuskey@users.noreply.github.com>
2020-11-19 15:17:40 -0500

    Error & Message Styling
    • Fixed the visual formatting of error messages by changing the styling of list items in the footer
    • Improved the visual style of messages
    • Added a "footer-list" class to `private/shared/public_footer.php` and `private/shared/staff_footer.php`

--- private/shared/staff_footer.php
+++ private/shared/staff_footer.php
@@ -6,9 +6,9 @@
               <p id="copyright">&copy; <?php echo date('Y'); ?> Matthew Huskey</p>
               
               <ul class="text-center">
-                <li id="github"><a href="https://github.com/mhuskey" target="_blank"><i class="fab fa-github"></i></a></li>
-                <li id="mail"><a href="mailto:matthewhuskey@me.com"><i class="fas fa-envelope"></i></a></li>
-                <li id="linkedin"><a href="https://www.linkedin.com/in/matthew-huskey-17258716b" target="_blank"><i class="fab fa-linkedin"></i></a></li>
+                <li class="footer-list" id="github"><a href="https://github.com/mhuskey" target="_blank"><i class="fab fa-github"></i></a></li>
+                <li class="footer-list" id="mail"><a href="mailto:matthewhuskey@me.com"><i class="fas fa-envelope"></i></a></li>
+                <li class="footer-list" id="linkedin"><a href="https://www.linkedin.com/in/matthew-huskey-17258716b" target="_blank"><i class="fab fa-linkedin"></i></a></li>
               </ul>
             </div>
           </div>
